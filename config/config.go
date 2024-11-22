54f64cbb4f4b573bb006f3cd645fb63d3e91d4d3
Javier Acevedo C <javier.iacevedoc@gmail.com>
2024-11-22 00:54:24 -0300

    feat(config): basic config, depends on .env file and create a google pass for application

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	SMTPHost     string
+	SMTPPort     int
+	SMTPUser     string
+	SMTPPassword string
+}
+
+var AppConfig *Config
+
+func LoadConfig() {
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+
+	AppConfig = &Config{
+		SMTPHost:     viper.GetString("SMTP_HOST"),
+		SMTPPort:     viper.GetInt("SMTP_PORT"),
+		SMTPUser:     viper.GetString("SMTP_USER"),
+		SMTPPassword: viper.GetString("SMTP_PASSWORD"),
+	}
+}
