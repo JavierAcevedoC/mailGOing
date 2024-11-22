5107985b2897920a0bf846a68f0a314deb3f1767
Javier Acevedo C <javier.iacevedoc@gmail.com>
2024-11-22 01:00:25 -0300

    fix(config): its require an google(in my case) password application

--- config/config.go
+++ config/config.go
@@ -28,6 +28,6 @@ func LoadConfig() {
 		SMTPHost:     viper.GetString("SMTP_HOST"),
 		SMTPPort:     viper.GetInt("SMTP_PORT"),
 		SMTPUser:     viper.GetString("SMTP_USER"),
-		SMTPPassword: viper.GetString("SMTP_PASSWORD"),
+		SMTPPassword: viper.GetString("SMTP_PASSWORD"), // para esto fue necesario usar el password de aplicaciones de google
 	}
 }
